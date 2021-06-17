b6627607cd24703ca1a638b1e8dbd0c113853657
Rokas <rookas.rudzenskas@gmail.com>
2021-06-17 15:53:50 +0300

    Roaks

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -1,10 +1,21 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import {Welcome} from "../screens/welcome";
 
 export default function RootNavigation() {
+    const Stack = createStackNavigator();
+
+    const screenOptions = {
+        headerShown: false,
+    };
+
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                {/*     these are screens, we are going to be accessing*/}
+                <Stack.Screen name="Splash" component={Welcome} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
